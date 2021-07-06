732af4399143d556a27c64862d491ee8872272ae
nabesuke <nabesuke5098@gmail.com>
2021-07-06 18:27:36 +0900

    [modified]BookmarkController

--- src/app/Http/Controllers/Bookmarks/BookmarkController.php
+++ src/app/Http/Controllers/Bookmarks/BookmarkController.php
@@ -24,6 +24,7 @@ use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Facades\Validator;
 use Illuminate\Validation\ValidationException;
 use Illuminate\View\View;
+use App\Lib\LinkPreview\MockLinkPreview;
 
 class BookmarkController extends Controller
 {
@@ -127,7 +128,8 @@ class BookmarkController extends Controller
      */
     public function create(CreateBookmarkRequest $request)
     {
-        $useCase = new CreateBookmarkUseCase(new LinkPreview());
+        // $useCase = new CreateBookmarkUseCase(new LinkPreview());
+        $useCase = new CreateBookmarkUseCase(new MockLinkPreview());
         $useCase->handle($request->url, $request->category, $request->comment);
 
         // 暫定的に成功時は一覧ページへ
