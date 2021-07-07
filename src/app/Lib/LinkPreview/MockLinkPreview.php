be61dfc1c64bc2270c876ada11ecf2a2cdeae24e
nabesuke <nabesuke5098@gmail.com>
2021-07-08 08:29:40 +0900

    [add]tests CreateBookmarkUseCaseTest.php

--- src/app/Lib/LinkPreview/MockLinkPreview.php
+++ src/app/Lib/LinkPreview/MockLinkPreview.php
@@ -3,12 +3,12 @@ namespace App\Lib\LinkPreview;
 
 final class MockLinkPreview implements LinkPreviewInterface
 {
-    public function get(string $url): GetLinkPreviewResponse
-    {
-        return new GetLinkPreviewResponse(
-            'モックのタイトル',
-            'モックのdescription',
-            'https://i.gyazo.com/634f77ea66b5e522e7afb9f1d1dd75cb.png'
-        );
-    }
+  public function get(string $url): GetLinkPreviewResponse
+  {
+    return new GetLinkPreviewResponse(
+      'モックのタイトル',
+      'モックのdescription',
+      'https://i.gyazo.com/634f77ea66b5e522e7afb9f1d1dd75cb.png'
+    );
+  }
 }
